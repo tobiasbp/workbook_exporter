5bab0ef0e876ba31ced07293f07dcaa5b5498e7f
tobiasbp <tobiasbp@gmail.com>
2020-03-20 08:06:43 +0100

    A few metrics exported. Way too many labels in use. Must be fixed.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # workbook_exporter
 A Prometheus exporter of metrics from Deltek Workbook
+
+No of active jobs for a customer
+workbook_customer_active_jobs{
+  customer_id="220",
+  name="Example Inc.",
+  payment_term_id="24",
+  responsible_employee_id="21",
+  type_id="1"
+  } 5.0
+
+No of days since job was created
+workbook_job_age_days{
+  billable="0",
+  company_id="1",
+  created="2011-10-18T15:39:27.493Z",
+  customer_id="5",
+  customer_name="Example Inc.",
+  days_to_end_date="286",
+  department_id="1",
+  department_name="Example department",
+  job_id="1000003",
+  name="Example job",
+  retainer="0",
+  status_id="1",
+  type_id="15"
+  } 3074.0
+
+Days an employee has been employed
+workbook_employee_days_employed{
+  company_id="1",
+  department_id="2",
+  employee_id="100",
+  employment_type_id="1",
+  position_id="2",
+  sex_id="1",
+  type_id="1"
+  } 688.0
+
+Did we get data from Workbook without errors (0.0|1.0)?
+workbook_up 1.0
---
+++ workbook_exporter.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python3
+
+import argparse
+from datetime import datetime
+import os
+import random
+import time
+
+from prometheus_client import start_http_server, Summary
+from prometheus_client.core import GaugeMetricFamily, REGISTRY
+import workbook_api
+
+# The string to use when converting times in Workbook
+# Example: 2020-08-17T09:02:23.677Z
+TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+# Jobs with status id in this list, are reported
+ACTIVE_JOBS = [0,1]
+
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+
+# Decorate function with metric.
+@REQUEST_TIME.time()
+def process_request(t):
+    """A dummy function that takes some time."""
+    time.sleep(t)
+
+
+def parse_date(wb_time):
+    '''
+    Convert a Workbook time string to a datetime object
+    '''
+    return datetime.strptime(wb_time, TIME_FORMAT)
+    
+
+class WorkbookCollector(object):
+
+  def __init__(self, wb_url, wb_user, wb_pass):
+      # Workbook API object
+      self.wb = workbook_api.WorkbookAPI(wb_url, wb_user, wb_pass)
+
+  def collect(self):
+
+    # Assume no problems with getting data from Workbook
+    wb_error = False
+
+    # A dictionary with customer_id as keys
+    wb_customers = {}
+    
+    # Get the Workbook departments
+    departments = {}
+    try:
+        result = self.wb.get_departments()
+    except Exception as e:
+        print("Error getting departments from WB: {}".format(e))
+        wb_error = True
+    else:
+        # Dict of departments with company ID is key
+        departments = {d['CompanyId']:{} for d in result}
+        for d in result:
+            departments[d['CompanyId']][d['Id']] = d['Name']
+        print(departments)
+
+    # JOBS #
+
+    job_age_days = GaugeMetricFamily(
+        'workbook_job_age_days',
+        'Days since job was created',
+        labels=[
+            "job_id",
+            "name",
+            "status_id",
+            "company_id",
+            "created",
+            "type_id",
+            "customer_id",
+            "customer_name",
+            "department_id",
+            "department_name",
+            "retainer",
+            "billable",
+            "days_to_end_date",
+            ])
+
+
+    try:
+        result = self.wb.get_jobs(Status=ACTIVE_JOBS)
+    except Exception as e:
+        print(e)
+        wb_error = True
+    else:
+        for j in result:
+            
+            # Maintain dict with no of jobs pr. customer
+            if not j.get('CustomerId') in wb_customers.keys():
+                wb_customers[j.get('CustomerId')] = {'no_of_jobs': 0}
+            else:
+                wb_customers[j.get('CustomerId')]['no_of_jobs'] += 1
+                
+            # Time job was created
+            date_created = parse_date(j.get('CreateDate'))
+            
+            # End date for job
+            date_end = parse_date(j.get('EndDate'))
+            
+            # Add the metric(lables, value)
+            job_age_days.add_metric(
+                [str(j.get('Id')),
+                    j.get('JobName'),
+                    str(j.get('StatusId')),
+                    str(j.get('CompanyId')),
+                    str(j.get('CreateDate')),
+                    str(j.get('JobTypeId')),
+                    str(j.get('CustomerId')),
+                    str(j.get('CustomerName')),
+                    str(j.get('CompanyDepartmentId')),
+                    departments[j.get('CompanyId')][j.get('CompanyDepartmentId')],
+                    "1" if j.get('RetainerJob') else "0",
+                    "1" if j.get('Billable') else "0",
+                    str((date_end - datetime.today()).days)
+                    ],
+                # Days since creation
+                (datetime.today() - date_created).days
+                )
+    
+    yield job_age_days
+
+
+    # EMPLOYEES #
+
+    employee_days_employed = GaugeMetricFamily(
+        'workbook_employee_days_employed',
+        'Days since the employee was hired',
+        labels=[
+            "company_id",
+            "employee_id",
+            "department_id",
+            "type_id",
+            "sex_id",
+            "position_id",
+            "employment_type_id",
+            ])
+
+    try:
+        # Get active employees
+        employees = self.wb.get_employees(Active=True)
+    except Exception:
+        print("Could not get WB employees with error: {}".format(e))
+        wb_error = True
+    else:
+        for e in employees:
+            employee_days_employed.add_metric(
+                [
+                    str(e.get('CompanyId', '')),
+                    str(e.get('Id', '')),
+                    str(e.get('DepartmentId', '')),
+                    str(e.get('TypeId', '')),
+                    str(e.get('Sex', '')),
+                    str(e.get('EmployeePosition', '')),
+                    str(e.get('EmploymentTypeId', '')),
+                ],
+                (datetime.today() - parse_date(e['HireDate'])).days
+                )
+
+        yield employee_days_employed
+
+    # CUSTOMERS #
+
+    customers = GaugeMetricFamily(
+        'workbook_customer_active_jobs',
+        'No of active jobs for Workbook customer',
+        labels=[
+            "customer_id",
+            "type_id",
+            "responsible_employee_id",
+            "payment_term_id",
+            "name",
+            ]
+        )
+
+    try:
+        result = self.wb.get_costumers(Active=True)
+    except Exception as e:
+        print("Could not get customers with error: {}".format(e))
+    else:
+        for c in result:
+
+            try:
+                n = wb_customers[c['Id']]['no_of_jobs']
+            except KeyError:
+                n = 0
+            
+            customers.add_metric(
+                [
+                    str(c.get('Id', '')),
+                    str(c.get('TypeId', '')),
+                    str(c.get('ResponsibleEmployeeId', '')),
+                    str(c.get('PaymentTermId', '')),
+                    str(c.get('Name', '')),
+                ],
+                n
+            )
+
+
+        yield customers
+    
+    #print(wb_customers)
+    
+    # PROBLEMS WITH WORKBOOK? #
+
+    metric = GaugeMetricFamily(
+        'workbook_up',
+        'Is data beeing pulled from Workbook'
+        )
+
+    if wb_error:
+        metric.add_metric([], 0)
+    else:
+        metric.add_metric([], 1)
+
+    yield metric
+
+
+def parse_args():
+    '''
+    Parse the command line arguments
+    '''
+
+    # Parser object
+    parser = argparse.ArgumentParser(
+        description='Exports data from Workbook to be consumed by Prometheus'
+    )
+
+    # The Workbook URL
+    parser.add_argument(
+        '--workbook-url',
+        #metavar='wb_url',
+        required=False,
+        help='Server url for the Workbook API',
+        default=os.environ.get('WORKBOOK_URL', 'https://example.workbook.dk')
+    )
+
+    # The Workbook user
+    parser.add_argument(
+        '--workbook-user',
+        required=False,
+        help='User name for logging in to Workbook',
+        default=os.environ.get('WORKBOOK_USER', 'workbook-user')
+    )
+
+    # The Workbook password
+    parser.add_argument(
+        '--workbook-password',
+        required=False,
+        help='Password to for logging in to Workbook',
+        default=os.environ.get('WORKBOOK_PASSWORD', 'workbook-password')
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    
+    try:
+        # Parse the command line arguments
+        args = parse_args()
+
+        REGISTRY.register(
+            WorkbookCollector(
+                args.workbook_url,
+                args.workbook_user,
+                args.workbook_password
+                )
+            )
+        
+        # Start up the server to expose the metrics.
+        start_http_server(8000)
+        
+        # Generate some requests.
+        while True:
+            process_request(random.random())
+
+    except KeyboardInterrupt:
+        print(" Interrupted by keyboard")
+        exit(0)
+
+
+if __name__ == '__main__':
+    main()
